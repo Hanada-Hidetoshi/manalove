a93d12838b1a02f759043fc7c93a2cb9deb00e20
Hanada-Hidetoshi <hideeuph@icloud.com>
2021-03-16 15:28:28 +0000

    error check

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -10,13 +10,17 @@ use Session;
 class TopController extends Controller
 {
     function index(){
-        $p_subjects = \App\Models\SubjectData::where('classfication', 1)->get();
-        $j_h_subjects = \App\Models\SubjectData::where('classfication', 2)->get();
-        $h_subjects = \App\Models\SubjectData::where('classfication', 3)->get();
-        $s_subjects = \App\Models\SubjectData::where('classfication', 4)->get();
-        $creates = \App\Models\UserData::where('user_attribute', 0)->orderBy('created_at', 'desc')->take(3)->get();
-        $updates = \App\Models\UserData::where('user_attribute', 0)->orderBy('updated_at', 'desc')->take(3)->get();
-        $specials = \App\Models\UserData::where('user_attribute', 0)->whereNotNull('s_subject')->orderBy('updated_at', 'desc')->take(3)->get();
+        if(\App\Models\SubjectData::exists()){
+            $p_subjects = \App\Models\SubjectData::where('classfication', 1)->get();
+            $j_h_subjects = \App\Models\SubjectData::where('classfication', 2)->get();
+            $h_subjects = \App\Models\SubjectData::where('classfication', 3)->get();
+            $s_subjects = \App\Models\SubjectData::where('classfication', 4)->get();
+        }
+        if(\App\Models\UserData::exists()){
+            $creates = \App\Models\UserData::where('user_attribute', 0)->orderBy('created_at', 'desc')->take(3)->get();
+            $updates = \App\Models\UserData::where('user_attribute', 0)->orderBy('updated_at', 'desc')->take(3)->get();
+            $specials = \App\Models\UserData::where('user_attribute', 0)->whereNotNull('s_subject')->orderBy('updated_at', 'desc')->take(3)->get();
+        }
         return view('top',compact('creates','updates','specials','p_subjects','j_h_subjects','h_subjects','s_subjects'));
     }
 }
