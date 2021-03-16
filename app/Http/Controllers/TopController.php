501c81a3309a0397b21f3009508df9f80b45e4e6
Hanada-Hidetoshi <hideeuph@icloud.com>
2021-03-16 15:31:31 +0000

    error check

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -15,11 +15,20 @@ class TopController extends Controller
             $j_h_subjects = \App\Models\SubjectData::where('classfication', 2)->get();
             $h_subjects = \App\Models\SubjectData::where('classfication', 3)->get();
             $s_subjects = \App\Models\SubjectData::where('classfication', 4)->get();
+        }else{
+            $p_subjects = [];
+            $j_h_subjects =  [];
+            $h_subjects = [];
+            $s_subjects = [];
         }
         if(\App\Models\UserData::exists()){
             $creates = \App\Models\UserData::where('user_attribute', 0)->orderBy('created_at', 'desc')->take(3)->get();
             $updates = \App\Models\UserData::where('user_attribute', 0)->orderBy('updated_at', 'desc')->take(3)->get();
             $specials = \App\Models\UserData::where('user_attribute', 0)->whereNotNull('s_subject')->orderBy('updated_at', 'desc')->take(3)->get();
+        }else{
+            $creates =[];
+            $updates =[];
+            $specials =[];
         }
         return view('top',compact('creates','updates','specials','p_subjects','j_h_subjects','h_subjects','s_subjects'));
     }
